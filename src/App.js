7c22bfef2d47859168b452010a640e71958f19b1
phycsepy <kundancseb2@vjit.ac.in>
2021-08-01 20:01:29 +0530

    first-part

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+function Heading(props){
+  return(
+  <h1>First look to ReactJS & i am {props.Name}</h1>
+  );
+}
+
+
+
+function Main(props){
+  return(
+    <div>
+    <h2>Point number {props.number} It is all about learning and interest and i have mixed feelings on ReactJS</h2>
+    <img src='https://github.com/phycsepy.png' alt='ironman'/>
+    <ul style={{textAlign : "left"}}>{
+      props.dishes.map(dish =>(
+        <li key={dish.id} >{dish.title}</li>
+      ))
+    }
+
+    </ul>
+    </div>
+  );
+}
+
+function Footer(){
+  return (
+    <h3> Copy Rights 2021 </h3>
+  );
+}
+
+const dishes = [
+  "poori","samosa","franky"
+]
+
+
+// dishes.map(dish=>console.log(dish))
+const dishesObj = dishes.map((dish,i)=>({id:i,title:dish}))
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Heading Name="Kundan"/>
+    <Main number="one" dishes={dishesObj}/>
+    <Footer/>
     </div>
   );
 }
