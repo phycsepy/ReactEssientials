991bc44c8ee0b1f58ac1602723385d720c1ac5bf
phycsepy <kundancseb2@vjit.ac.in>
2021-08-02 07:19:36 +0530

    secound addups

--- src/App.js
+++ src/App.js
@@ -1,51 +1,19 @@
-import './App.css';
-
-
-function Heading(props){
+function NeedHelp(){
   return(
-  <h1>First look to ReactJS & i am {props.Name}</h1>
+    <h1>need help please </h1>
   );
 }
-
-
-
-function Main(props){
+function NoNeedHelp(){
   return(
-    <div>
-    <h2>Point number {props.number} It is all about learning and interest and i have mixed feelings on ReactJS</h2>
-    <img src='https://github.com/phycsepy.png' alt='ironman'/>
-    <ul style={{textAlign : "left"}}>{
-      props.dishes.map(dish =>(
-        <li key={dish.id} >{dish.title}</li>
-      ))
-    }
-
-    </ul>
-    </div>
+    <h1>No need help</h1>
   );
 }
 
-function Footer(){
+function App({condition}) {
   return (
-    <h3> Copy Rights 2021 </h3>
-  );
-}
-
-const dishes = [
-  "poori","samosa","franky"
-]
-
-
-// dishes.map(dish=>console.log(dish))
-const dishesObj = dishes.map((dish,i)=>({id:i,title:dish}))
-
-function App() {
-  return (
-    <div className="App">
-    <Heading Name="Kundan"/>
-    <Main number="one" dishes={dishesObj}/>
-    <Footer/>
-    </div>
+    <>
+    {condition?<NeedHelp/>:<NoNeedHelp/>}
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import './index.css';
 import App from './App'
 
 ReactDOM.render(
-  <App/>,
+  <App condition={true}/>,
   document.getElementById('root')
 );
