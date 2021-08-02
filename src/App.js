fa5825b0cc489997f16dc4e783b269a95acf243e
phycsepy <kundancseb2@vjit.ac.in>
2021-08-02 08:58:49 +0530

    first two hookes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-function NeedHelp(){
-  return(
-    <h1>need help please </h1>
-  );
-}
-function NoNeedHelp(){
-  return(
-    <h1>No need help</h1>
-  );
-}
+import React, {useState,useEffect}from "react"
+function App() {
+  const [emotion,setEmotion]=useState('happy')
+  const [emotionTwo,setEmotiontwo] = useState('tired')
+  useEffect(()=>{
+    console.log(`it detects only change in state of 1nd emotion `);
+  },[emotion])
 
-function App({condition}) {
+  useEffect(()=>{
+    console.log(`it detects only change in state of 2nd emotion `);
+  },[emotionTwo])
+
+
   return (
     <>
-    {condition?<NeedHelp/>:<NoNeedHelp/>}
+      <h1>Emotion detecter my state {emotion}and{emotionTwo}</h1>
+      <button onClick={()=>setEmotion('sad')}>sad
+      </button>
+      <button onClick={()=>setEmotion('happy')}>happy
+      </button>
+      <button onClick={()=>setEmotiontwo('creepy')}>creekpy
+      </button>
+      <button onClick={()=>setEmotiontwo('tired')}>tired
+      </button>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import './index.css';
 import App from './App'
 
 ReactDOM.render(
-  <App condition={true}/>,
+  <App/>,
   document.getElementById('root')
 );
